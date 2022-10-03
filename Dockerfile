5f724a6f99a52a381d92de9019af91a2d5e6321e
yangtian <yangtian9999@163.com>
2022-10-03 11:53:25 +0800

    first

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:8-slim
+
+ADD ./jars/*.jar /home/
---
+++ copy-jars.sh
@@ -0,0 +1,8 @@
+cp mall-admin/target/*.jar ./jars
+cp mall-common/target/*.jar ./jars
+cp mall-demo/target/*.jar ./jars
+cp mall-mbg/target/*.jar ./jars
+cp mall-portal/target/*.jar ./jars
+cp mall-search/target/*.jar ./jars
+cp mall-security/target/*.jar ./jars
+
